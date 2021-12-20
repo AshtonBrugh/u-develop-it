7427a9b7200296b7e4e970948fe26f4b6c38effb
Ashton B <ashtonbrugh@gmail.com>
2021-12-20 17:10:25 -0500

    building database calls

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,10 +7,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+//Connect to database
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        //MySQL username,
+        user: 'AshtonBrugh',
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
 // Default response for any other request (Not found)
 app.use ((req, res) => {
     res.status(404).end();
-})
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
